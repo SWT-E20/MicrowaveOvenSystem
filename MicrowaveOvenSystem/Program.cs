36cea22f7f3f879847ac13d118fcb993e98fadfe
mvraa <40834090+mvraa@users.noreply.github.com>
2020-11-11 09:30:41 +0100

    project

---
+++ MicrowaveOvenSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicrowaveOvenSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
