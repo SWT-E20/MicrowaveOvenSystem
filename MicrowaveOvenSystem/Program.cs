c1fc902c1b276b56070c1321e572458fcec2d093
mvraa <40834090+mvraa@users.noreply.github.com>
2020-11-11 09:36:44 +0100

    gitignore virker
    test af gitignore

--- MicrowaveOvenSystem/Program.cs
+++ MicrowaveOvenSystem/Program.cs
@@ -7,6 +7,7 @@ namespace MicrowaveOvenSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test af at gitignore virker.");
         }
     }
 }
