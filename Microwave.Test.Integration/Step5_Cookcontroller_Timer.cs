502d2493f5d61116de38ab96be210fc146d8f53d
mvraa <40834090+mvraa@users.noreply.github.com>
2020-12-02 13:46:03 +0100

    error in time boundary discovered with integration test

--- Microwave.Test.Integration/Step5_Cookcontroller_Timer.cs
+++ Microwave.Test.Integration/Step5_Cookcontroller_Timer.cs
@@ -12,42 +12,58 @@ namespace Microwave.Test.Integration
     [TestFixture]
     class Step5_Cookcontroller_Timer
     {
-        //private CookController _cookController;
+        private Door _door;
+        private Button _pButton, _tButton, _scButton;
+        private UserInterface _userInterface;
+        private Light _light;
+        private IDisplay _display;
+        private CookController _cookController;
+        private Timer _timer;
+        private PowerTube _powerTube;
+        private IOutput _output;
 
-        //private IPowerTube _powerTube;
-        //private IDisplay _display;
-        //private IUserInterface _userInterface;
-        //private IButton _sbutton;
+        [SetUp]
+        public void Setup()
+        {
+            _output = Substitute.For<IOutput>();
+            _display = Substitute.For<IDisplay>();
 
-        //private ITimer _timer;
-        //private IOutput _output;
+            _door = new Door();
+            _pButton = new Button();
+            _tButton = new Button();
+            _scButton = new Button();
+            _timer = new Timer();
+            _powerTube = new PowerTube(_output);
+            _light = new Light(_output);
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_pButton, _tButton, _scButton, _door, _display, _light, _cookController);
 
+            _cookController.UI = _userInterface;
+        }
 
-        //[SetUp]
-        //public void Setup()
-        //{
-        //    _output = Substitute.For<IOutput>();
-        //    _timer = Substitute.For<ITimer>();
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        public void StartCooking_TimerRemainingEqualsTime(int time)
+        {
+            _cookController.StartCooking(50, time);
+            Assert.AreEqual(_timer.TimeRemaining, time);
+        }
 
-        //    _sbutton = new Button();
+        [TestCase(5, 4, 0, 1)]
+        [TestCase(10, 5, 0, 5)]
+        [TestCase(60, 10, 0, 50)]
+        [TestCase(120, 5, 1, 55)]
+        [TestCase(180, 10, 2, 50)]
+        public void StartCooking(int time, int timeElapsed, int minPassed, int secPassed)
+        {
+            _cookController.StartCooking(50, time);
 
-        //    _display = new Display(_output);
-        //    _powerTube = new PowerTube(_output);
+            System.Threading.Thread.Sleep(timeElapsed*1000 + 500); // plus 500 just to make sure event is thrown
 
-        //    _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
-        //}
-        //[TestCase(1, 5)]
-        //[TestCase(50, 5)]
-        //[TestCase(100, 5)]
-        //[TestCase(350, 5)]
-        //[TestCase(650, 5)]
-        //[TestCase(699, 5)]
-        //[TestCase(700, 5)]
-        //public void StartCooking_CorrectPower(int power, int time)
-        //{
-        //    _sbutton.Press();
-        //    _cookController.StartCooking(power, time);
-        //    _output.Received().OutputLine(Arg.Is<string>(str => str.Contains($"{power}")));
-        //}
+            _display.Received(1).ShowTime(minPassed, secPassed);
+        }
     }
 }
